f8cfaa464fc8700fef594c32ab4ee36b0117b694
conto <constantijn@vanhartesveldt.nl>
2021-02-16 10:40:58 +0100

    working on the front end because tom didnt do shit

--- website/databasescripts/edit_event.php
+++ website/databasescripts/edit_event.php
@@ -36,45 +36,5 @@ $row = $result->fetch_assoc();
             <!--TODO make permission system-->
             <input type="submit">
         </form>
-<?php 
-if (isset($_FILES['image'])) {
-    $errors= array();
-    $file_name = $_FILES['image']['name'];
-    $file_size = $_FILES['image']['size'];
-    $file_tmp = $_FILES['image']['tmp_name'];
-    $file_type = $_FILES['image']['type'];
-    $file_ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
-    
-    $extensions= array("jpg");
-    
-    if (in_array($file_ext,$extensions)=== false) {
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-    }
-    
-    // if($file_size > 2097152){
-    //    $errors[]='File size must be excately 2 MB';
-    // }
-    
-    if (empty($errors) == true) {
-        //upload for the file.
-        move_uploaded_file($file_tmp,$_SERVER['DOCUMENT_ROOT']."../CDN/IMG/".$file_name);
-
-        // // Formuleer query
-        // $sql = "INSERT INTO `fotoalbums` (`foto`) VALUES ('{$file_name}')";
-        // // Poging uitvoeren query
-        // if ($conn->query($sql) === TRUE) {
-        //     // Uitvoeren query gelukt
-        //     echo "Nieuwe profielfoto succesvol toegevoegd aan tabel 'foto'.";
-        // } else {
-        //     // Uitvoeren query mislukt
-        //     echo "Error: " . $sql . "<br>" . $conn->error;
-        // }
-        // Afsluiten verbinding
-        $conn->close();
-
-    } else {
-        print_r($errors);
-    }
-}
-
+<?php
 closecon($conn);?>
